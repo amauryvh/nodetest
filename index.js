b88bc1fff2d756e8d46b9029087a8e21c6e6382b
Amaury VH <avhernandezx@gmail.com>
2018-04-30 00:44:14 -0400

    Files to start a web page

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //https://www.nodebeginner.org/index-es.html
 var server = require("./server");
-server.iniciar();
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
+
+server.iniciar(router.route, handle);
