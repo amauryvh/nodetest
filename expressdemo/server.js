dd611b3b8f25a4947252975aa626cde2d07f1fa2
Amaury Viera <aviera@qualitydistribution.com>
2018-05-01 13:26:06 -0400

    Including expressdemo

---
+++ expressdemo/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+/*
+app.get('/', function(req, res) {
+  res.send('Hello World')
+})
+*/
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+*/
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
